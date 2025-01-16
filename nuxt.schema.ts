09f8042bfadb53f181fd8b914ee0ad32df1092d7
Fabien Ungerer <fabien.ungerer@gmail.com>
2025-01-16 11:42:25 +0100

    simple setup

--- nuxt.schema.ts
+++ nuxt.schema.ts
@@ -1,15 +1,17 @@
 import { field, group } from '@nuxthq/studio/theme'
 
 export default defineNuxtSchema({
-  ui: group({
-    title: 'UI',
-    description: 'UI description',
-    fields: {
-      test: field({
-        type: 'string',
-        title: 'Test',
-        description: 'Test de field',
-      }),
-    },
-  }),
+  appConfig: {
+    ui: group({
+      title: 'UI',
+      description: 'UI description',
+      fields: {
+        test: field({
+          type: 'string',
+          title: 'Test',
+          description: 'Test de field',
+        }),
+      },
+    }),
+  },
 })
