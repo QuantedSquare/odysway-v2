c89574bb70fe414fdd90afa402280ca9489744c6
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2025-02-04 08:07:44 +0100

    app bar in progress

---
+++ components/HeaderAppBar.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-app-bar>
+    <v-app-bar-title>
+      <NuxtLink to="/">
+        <v-img
+          width="150px"
+          max-height="48px"
+          src="/logos/logo_couleur.png"
+          contain
+        />
+      </NuxtLink>
+    </v-app-bar-title>
+    <v-spacer />
+    <v-btn icon>
+      <v-icon>{{ mdiMagnify }}</v-icon>
+    </v-btn>
+    <v-btn icon>
+      <v-icon>{{ mdiDotsVertical }}</v-icon>
+    </v-btn>
+  </v-app-bar>
+</template>
+
+<script setup>
+import { mdiMagnify, mdiDotsVertical } from '@mdi/js'
+</script>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,6 +1,24 @@
 <template>
   <v-app>
-    <v-app-bar>Odysway</v-app-bar>
+    <v-app-bar>
+      <v-app-bar-title>
+        <NuxtLink to="/">
+          <v-img
+            width="150px"
+            max-height="48px"
+            src="/logos/logo_couleur.png"
+            contain
+          />
+        </NuxtLink>
+      </v-app-bar-title>
+      <v-spacer />
+      <v-btn icon>
+        <v-icon>{{ mdiMagnify }}</v-icon>
+      </v-btn>
+      <v-btn icon>
+        <v-icon>{{ mdiDotsVertical }}</v-icon>
+      </v-btn>
+    </v-app-bar>
     <v-main>
       <slot />
     </v-main>
@@ -9,3 +27,7 @@
     </v-footer>
   </v-app>
 </template>
+
+<script setup>
+import { mdiMagnify, mdiDotsVertical } from '@mdi/js'
+</script>
