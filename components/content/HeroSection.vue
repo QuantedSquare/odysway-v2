3941996201e2c6ef30c055c1bf77274e23ad5257
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2025-02-24 09:13:22 +0100

    test lighthouse

--- components/content/HeroSection.vue
+++ components/content/HeroSection.vue
@@ -5,6 +5,8 @@
   <v-img
     :src="img(imageSrc, { format: 'webp', quality: 70, height: 900 })"
     :lazy-src="img(imageSrc, { format: 'webp', quality: 10, height: 900 })"
+    size="(max-width: 600) 480px, 1500px"
+    :srcset="`${img(imageSrc, { format: 'webp', quality: 70, width: 600 })} 480w, ${img(imageSrc, { format: 'webp', quality: 70, width: 900 })} 1500w`"
     height="100vh"
     cover
   >
