efdb6a2224300560413c545748b1d876cca140af
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2025-02-27 15:55:29 +0100

    WIP: blog page

---
+++ components/content/blogs/BlogCol.vue
@@ -0,0 +1,14 @@
+<template>
+  <v-col
+    cols="12"
+    sm="6"
+  >
+    <v-card variant="text">
+      <v-img src="/images/AdobeStock_124472096.jpeg">
+        <div class="position-absolute bottom-0" />
+      </v-img>
+      <v-card-subtitle>test</v-card-subtitle>
+      <v-card-title>TEST</v-card-title>
+    </v-card>
+  </v-col>
+</template>
---
+++ components/content/blogs/BlogContainer.vue
@@ -0,0 +1,19 @@
+<template>
+  <v-container>
+    <v-row>
+      <slot name="blog-col" />
+    </v-row>
+  </v-container>
+</template>
+
+<script setup>
+const route = useRoute()
+
+const { data: pages } = await useAsyncData('', () => {
+  return queryCollection('content').path(`${route.path}/blogs`).all()
+})
+
+console.log(pages.value)
+
+console.log(route.path)
+</script>
