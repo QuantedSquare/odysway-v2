59ae2033a1f22dfe8a049b9dbcf133218efaf502
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2025-03-31 17:31:27 +0200

    feat(Mentions legales): create page mentions legales; style adjustement

---
+++ components/content/PoliciesContainer.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-container>
+    <v-row>
+      <v-col
+        cols="12"
+        class="text-center"
+      >
+        <slot name="title" />
+      </v-col>
+      <v-col cols="12">
+        <slot name="text" />
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
