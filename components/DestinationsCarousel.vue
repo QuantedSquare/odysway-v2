ac70cb2bf0baf9c859b122ce5cd3dc63fc9523f7
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2025-03-31 09:28:16 +0200

    wip(DestinationsCarousel)

--- components/DestinationsCarousel.vue
+++ components/DestinationsCarousel.vue
@@ -13,6 +13,7 @@
         variant="outlined"
         :disabled="arrivedState.left"
         class="mr-2"
+        :class="displayScrollBtn ? 'd-inline' : 'd-none'"
         @click="x -= scrollAmount"
       >
         <v-icon
@@ -40,6 +41,7 @@
         color="primary"
         variant="outlined"
         :disabled="arrivedState.right"
+        :class="displayScrollBtn ? 'd-inline' : 'd-none'"
         @click="x += scrollAmount"
       >
         <v-icon
@@ -53,22 +55,43 @@
 
 <script setup>
 import { mdiChevronLeft, mdiChevronRight } from '@mdi/js'
-import { useScroll } from '@vueuse/core'
+import { useScroll, useElementSize } from '@vueuse/core'
 
-const scrollContainer = ref(null)
+const scrollContainer = useTemplateRef('scrollContainer')
 const scrollElement = ref(null)
+const nbScrollElementChildren = ref(null)
+const { x, arrivedState } = useScroll(scrollElement, { behavior: 'smooth' })
+const { width: scrollElementWidth } = useElementSize(scrollElement)
+
+const route = useRoute()
 
 onMounted(() => {
   nextTick(() => {
+    console.log('scroll element ', scrollContainer.value.$el)
     scrollElement.value = scrollContainer.value.$el
+    console.log('scroll element ', scrollElement.value)
+    console.log('scroll element children ', scrollElement.value.children)
+    nbScrollElementChildren.value = scrollElement.value.children.length
+    console.log('scroll element children ', nbScrollElementChildren.value)
   })
 })
-
-const { x, arrivedState } = useScroll(scrollElement, { behavior: 'smooth' })
+console.log('scroll element after unmounted ', scrollElement.value)
 
 const scrollAmount = computed(() => {
-  return 288
+  return 400
 })
+
+const displayScrollBtn = computed(() => {
+  console.log('scroll element width ', scrollElementWidth.value)
+  console.log('cumulate children width ', nbScrollElementChildren.value * 120)
+  console.log('show scroll btn ? ', displayScrollBtn.value)
+  return (nbScrollElementChildren.value * 120) > scrollElementWidth.value
+})
+
+// si x > 0 et new route.path => remettre x à 0
+function setXtoZero() {
+  x.value = 0
+}
 </script>
 
 <style scoped>
