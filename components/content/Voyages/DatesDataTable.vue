edfd4d31f73a27ca3532c2a9bb5a3e01bed5c57c
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2025-04-01 11:24:14 +0200

    style(DatesDataTable): add chip for voyages promos and refactor component code

--- components/content/Voyages/DatesDataTable.vue
+++ components/content/Voyages/DatesDataTable.vue
@@ -9,46 +9,65 @@
   >
     <template #headers="{ columns }">
       <tr>
-        <template
+        <th
           v-for="column in columns"
           :key="column.key"
+          class="text-center text-uppercase text-h6"
         >
-          <th class="text-center text-uppercase text-h6">
-            {{ column.title }}
-          </th>
-        </template>
+          {{ column.title }}
+        </th>
       </tr>
     </template>
+
     <template #[`item.departureDate`]="{ item }">
-      <div>
-        {{ dayjs(item.departureDate).locale('fr').format('ddd DD/MM/YYYY') }}
-      </div>
+      <div>{{ formatDate(item.departureDate) }}</div>
     </template>
+
     <template #[`item.returnDate`]="{ item }">
-      <div>
-        {{ dayjs(item.returnDate).locale('fr').format('ddd DD/MM/YYYY') }}
+      <div>{{ formatDate(item.returnDate) }}</div>
+    </template>
+
+    <template #[`item.price`]="{ item }">
+      <div
+        v-if="hasPromotion(item)"
+      >
+        <PromoChip>{{ getPromotionLabel(item) }}</PromoChip>
+        <div>{{ getDiscountedPrice(item) }} €</div>
+        <div class="text-decoration-line-through">
+          {{ item.price }} €
+        </div>
+      </div>
+      <div v-else>
+        <div>{{ item.price }} €</div>
       </div>
     </template>
+
     <template #[`item.bookedPlaces`]="{ item }">
-      <tr class="d-flex justify-end justify-lg-center">
-        <div
-          v-if="item.bookedPlaces < 2"
-        >
-          <span><v-icon> {{ mdiAccount }} </v-icon>Confirmé dès 2 inscrits</span>
+      <div class="d-flex justify-end justify-lg-center">
+        <div v-if="item.bookedPlaces < 2">
+          <span>
+            <v-icon>{{ mdiAccount }}</v-icon>
+            Confirmé dès 2 inscrits
+          </span>
         </div>
         <div
           v-else
           class="d-flex flex-column justify-center align-end align-lg-center ga-1"
         >
-          <span> <v-icon>
-            {{ mdiCheckCircleOutline }}
-          </v-icon> Départ garanti confirmé</span>
-          <span><v-icon>{{ mdiAccount }}</v-icon> {{ item.bookedPlaces }} inscrits - reste {{ item.maxTravellers - item.bookedPlaces }} places </span>
+          <span>
+            <v-icon>{{ mdiCheckCircleOutline }}</v-icon>
+            Départ garanti confirmé
+          </span>
+          <span>
+            <v-icon>{{ mdiAccount }}</v-icon>
+            {{ item.bookedPlaces }} inscrits - reste {{ item.maxTravellers - item.bookedPlaces }} places
+          </span>
         </div>
-      </tr>
+      </div>
     </template>
+
     <template #[`item.canBeBooked`]="{ item }">
-      <tr class="d-flex justify-end justify-lg-center">
+      <div class="d-flex justify-end justify-lg-center">
         <v-btn-secondary
           v-if="item.canBeBooked"
           :to="formatLink(item)"
@@ -60,7 +79,7 @@
         <div v-else>
           plus de places disponibles
         </div>
-      </tr>
+      </div>
     </template>
   </v-data-table>
 </template>
@@ -70,8 +89,8 @@ import { mdiAccount, mdiCheckCircleOutline } from '@mdi/js'
 import dayjs from 'dayjs'
 import customParseFormat from 'dayjs/plugin/customParseFormat.js'
 import 'dayjs/locale/fr'
-
 import { useDisplay } from 'vuetify'
+import PromoChip from '~/components/PromoChip.vue'
 
 const { smAndDown } = useDisplay()
 dayjs.extend(customParseFormat)
@@ -88,25 +107,50 @@ const headers = [
   { title: 'DATE RETOUR', key: 'returnDate' },
   { title: 'PRIX', key: 'price' },
   { title: 'ÉTAT', key: 'bookedPlaces' },
-  { title: 'RÉSERVER', key: 'canBeBooked' }]
+  { title: 'RÉSERVER', key: 'canBeBooked' },
+]
+
+const formatDate = (date) => {
+  return dayjs(date).locale('fr').format('ddd DD/MM/YYYY')
+}
+
+const hasPromotion = (item) => {
+  return item.promoEarlyBird || item.lastMinute
+}
+
+const getPromotionLabel = (item) => {
+  return item.promoEarlyBird ? 'Tarif Early Bird' : 'Tarif Last Minute'
+}
+
+const getDiscountedPrice = (item) => {
+  if (item.promoEarlyBird) {
+    return item.price - item.promoEarlyBird
+  }
+  if (item.lastMinute) {
+    return item.price - item.promoLastMinute
+  }
+  return item.price
+}
 
 const filteredDates = computed(() => {
-  return [...props.deal.dates || []].filter(date => dayjs(date.departureDate).isAfter()).sort((a, b) => {
-    return dayjs(a.departureDate).diff(dayjs(b.departureDate))
-  })
+  return [...props.deal.dates || []]
+    .filter(date => dayjs(date.departureDate).isAfter())
+    .sort((a, b) => dayjs(a.departureDate).diff(dayjs(b.departureDate)))
 })
 
 const tableItems = computed(() => {
-  return filteredDates.value.map((date) => {
-    return {
-      departureDate: date.departureDate,
-      returnDate: date.returnDate,
-      price: `${date.startingPrice || 0} €`,
-      bookedPlaces: date.bookedPlaces || 0,
-      maxTravellers: date.maxTravellers || 0,
-      canBeBooked: date.maxTravellers !== date.bookedPlaces,
-    }
-  })
+  return filteredDates.value.map(date => ({
+    departureDate: date.departureDate,
+    returnDate: date.returnDate,
+    price: date.startingPrice || 0,
+    bookedPlaces: date.bookedPlaces || 0,
+    maxTravellers: date.maxTravellers || 0,
+    canBeBooked: date.maxTravellers !== date.bookedPlaces,
+    earlyBird: date.earlyBird,
+    promoEarlyBird: date.promoEarlyBird || 0,
+    lastMinute: date.lastMinute,
+    promoLastMinute: date.promoLastMinute || 0,
+  }))
 })
 
 const formatLink = (date) => {
