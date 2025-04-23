ab66fce024511c54b94ec1d92fe9d9b21e44cae5
AlexandreOttmann <ottmann.alex@gmail.com>
2025-04-23 10:27:06 +0200

    added data config setup

--- components/content/PartenairesContainer.vue
+++ components/content/PartenairesContainer.vue
@@ -3,14 +3,22 @@
     class="d-flex justify-center"
     fluid
   >
-    <v-img
-      v-for="(partenaire, key) in partenaires"
-      :key="key"
-      :src="img(partenaire?.imgSrc, { format: 'webp', quality: 70, height: 32, width: 320 })"
-      :alt="`logo du partenaire ${partenaire.description}`"
-      height="50"
-      width="320"
-    />
+    <v-row>
+      <v-col
+        v-for="(partenaire, key) in partenaires"
+        :key="key"
+        cols="6"
+        :md="partenaires.length > 4 ? 2 : 3"
+      >
+        <v-img
+
+          :src="img(partenaire?.imgSrc, { format: 'webp', quality: 70, height: 32, width: 150 })"
+          :alt="`logo du partenaire ${partenaire.description}`"
+          height="50"
+          width="150"
+        />
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
