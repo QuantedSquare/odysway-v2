626d464cd4bd30e3b4cd02837e3eb8fc29ead58c
AlexandreOttmann <ottmann.alex@gmail.com>
2025-04-28 16:47:51 +0200

    fix & comments

--- components/content/blogs/BlogCardsContainer.vue
+++ components/content/blogs/BlogCardsContainer.vue
@@ -16,7 +16,7 @@
         <slot name="cta-button" />
       </v-col>
     </v-row>
-    <v-row>
+    <v-row v-if="mergedData">
       <v-col
         v-for="(blog, index) in mergedData"
         :key="index"
@@ -24,6 +24,7 @@
         sm="6"
         md="4"
       >
+        <!-- #todo changer par un v-bind & simplifier les noms de keys -->
         <BlogCard
           :blog-slug="blog.path"
           :blog-title="blog.title"
@@ -56,17 +57,9 @@ const props = defineProps({
 
 const { width } = useDisplay()
 
-const [
-  { data: blog1 },
-  { data: blog2 },
-  { data: blog3 },
-] = await Promise.all([
-  useAsyncData('blog1', () => queryCollection('blog').path(props.blogCardSlug1).first()),
-  useAsyncData('blog2', () => queryCollection('blog').path(props.blogCardSlug2).first()),
-  useAsyncData('blog3', () => queryCollection('blog').path(props.blogCardSlug3).first()),
+const mergedData = await Promise.all([
+  queryCollection('blog').path(props.blogCardSlug1).first(),
+  queryCollection('blog').path(props.blogCardSlug2).first(),
+  queryCollection('blog').path(props.blogCardSlug3).first(),
 ])
-
-const mergedData = computed(() => {
-  return [blog1.value, blog2.value, blog3.value]
-})
 </script>
