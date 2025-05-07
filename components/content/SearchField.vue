a0887c97b45a14c971d45a4734f05847bf058b25
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2025-05-07 11:36:32 +0200

    feat(voyage search): add search by destination, type, month

--- components/content/SearchField.vue
+++ components/content/SearchField.vue
@@ -5,10 +5,10 @@
         <v-row align="center">
           <v-col
             cols="12"
-            :md="isSearchPage ? 2 : 3"
+            md="3"
           >
             <v-autocomplete
-              v-model="destinationsChoices"
+              v-model="destinationChoice"
               label="Destinations"
               :items="destinations"
               clearable
@@ -18,20 +18,20 @@
           </v-col>
           <v-col
             cols="12"
-            :md="isSearchPage ? 2 : 3"
+            md="3"
           >
             <v-select
-              v-model="travelTypeChoices"
+              v-model="travelTypeChoice"
               :items="travelTypes"
               hide-details
               label="Type de voyage"
-              multiple
+              clearable
               :prepend-inner-icon="mdiTargetAccount"
             />
           </v-col>
           <v-col
             :cols="12"
-            :md="isSearchPage ? 2 : 3"
+            md="3"
           >
             <v-menu
               v-model="dateMenu"
@@ -59,32 +59,16 @@
                     multiple="range"
                     width="400"
                     format="dd/mm/YYYY"
+                    :min="new Date()"
+                    show-adjacent-months
                   />
                 </v-locale-provider>
               </v-card>
             </v-menu>
           </v-col>
-          <!-- Visible only on search page -->
-          <v-col
-            v-if="isSearchPage"
-            cols="12"
-            :md="isSearchPage ? 2 : 0"
-          >
-            <v-select
-              v-model="travelTypeChoices"
-              :items="travelTypes"
-              hide-details
-              label="Plus de filtres"
-              clearable
-              chips
-              closable-chips
-              multiple
-              :prepend-inner-icon="mdiTune"
-            />
-          </v-col>
           <v-col
             cols="12"
-            :md="isSearchPage ? 2 : 3"
+            md="3"
             class=" h-100"
           >
             <v-btn
@@ -104,64 +88,46 @@
 </template>
 
 <script setup>
-import { mdiMapMarkerOutline, mdiTargetAccount, mdiCalendarBlankOutline, mdiTune } from '@mdi/js'
+import { mdiMapMarkerOutline, mdiTargetAccount, mdiCalendarBlankOutline } from '@mdi/js'
 import dayjs from 'dayjs'
-import slugify from 'slugify'
 
 const router = useRouter()
-const route = useRoute()
 const dateMenu = ref(false)
 
 // Replace individual refs with useState
 const date = useState('searchDate', () => [])
-const travelTypeChoices = useState('searchTravelTypes', () => [])
-const destinationsChoices = useState('searchDestination', () => null)
 
-const destinations = ref([
-  { value: 'FR', title: 'France' },
-  { value: 'IT', title: 'Italie' },
-  { value: 'ES', title: 'Espagne' },
-  { value: 'PT', title: 'Portugal' },
-  { value: 'TR', title: 'Turquie' },
-  { value: 'GR', title: 'Grèce' },
+const travelTypeChoice = useState('searchTravelType', () => null)
+const destinationChoice = useState('searchDestination', () => null)
+
+const { data: destinationsv2 } = await useAsyncData('destinationsv2', () => {
+  return queryCollection('destinationsv2').first()
+})
+
+const destinations = computed(() => {
+  return destinationsv2.value.meta.body.map(d => d.name.charAt(0).toUpperCase() + d.name.slice(1))
+})
+
+const travelTypes = ref([
+  'Voyage individuel', 'Voyage en groupe',
 ])
-const travelTypes = [
-  'Tous', 'Voyage individuel', 'Voyage en famille', 'Voyage en couple', 'Voyage en groupe',
-]
 
 const formattedDate = computed(() => {
   return date.value ? dayjs(date.value[0]).format('DD/MM') + ' - ' + dayjs(date.value[date.value.length - 1]).format('DD/MM') : ''
 })
 
-const search = () => {
-  if (destinationsChoices.value?.length > 0) {
-    console.log('destinationsChoices.value', destinationsChoices.value)
-    router.push({
-      path: '/search',
-      query: {
-        destination: slugify(destinationsChoices.value, { lower: true }),
-        travelTypes: travelTypeChoices.value,
-        date: date.value ? `${dayjs(date.value[0]).format('YYYY-MM-DD')}-${dayjs(date.value[date.value.length - 1]).format('YYYY-MM-DD')}` : '',
-      },
-    })
-  }
+function search() {
+  router.push({
+    path: '/search',
+    query: {
+      destination: destinationChoice.value ? destinationChoice.value : null,
+      travelType: travelTypeChoice.value ? travelTypeChoice.value : null,
+      dateRange: date.value.length > 0 ? `${dayjs(date.value[0]).format('YYYY/MM/DD')}-${dayjs(date.value[date.value.length - 1]).format('YYYY/MM/DD')}` : null,
+    },
+  })
 }
-const isSearchPage = computed(() => {
-  return route.name === 'search'
-})
-// console.log(isSearchPage.value)
 </script>
 
-<!-- <style lang="css" scoped>
-.inner-textfield:deep(.v-field__overlay) {
-  background-color: rgba(255, 255, 255, 0.214)!important;
-  backdrop-filter: blur(8px);
-  box-shadow: 2px 2px 5px  rgba(255, 255, 255, 0.3);
-}
-.inner-textfield:deep(.v-field__field){
-  text-shadow: 3px 3px 5px rgba(0, 0, 0, 0.554);
-}
-</style> -->
 <style scoped>
 :deep(.v-icon__svg) {
   color: rgb(var(--v-theme-primary)) !important;
