17d531c126925cec9b30dad4dc02b29c1c72ecf6
AlexandreOttmann <ottmann.alex@gmail.com>
2025-07-18 11:47:38 +0200

    redesign top travels tab

--- components/content/TopTravelsTabs.vue
+++ components/content/TopTravelsTabs.vue
@@ -1,16 +1,16 @@
 <template>
   <v-container
     fluid
-    class="rounded-lg px-md-12 py-md-8 mt-4 mt-md-8 bg-primary max-container-width"
+    class="rounded-lg px-md-12 py-md-8 mt-4 mt-md-8 bg-primary max-container-width "
   >
     <div class="text-h3 font-weight-bold my-4">
       Des idées pour vos prochains voyages
     </div>
 
-    <div class="tabs-container position-relative mb-6">
+    <div class="tabs-container position-relative mb-6 mt-md-10 custom-color ">
       <v-tabs
         v-model="currentTab"
-        class="text-white-light"
+        class="text-white-light  "
         color="white-light"
         role="tablist"
         :aria-label="`Navigation des idées de voyages`"
@@ -21,10 +21,15 @@
           :key="index"
           :value="index"
           role="tab"
+          class="text-decoration-none pb-md-8 "
           :aria-selected="currentTab === index"
           :aria-controls="`tabpanel-${index}`"
+          :class="index === 0 ? 'pl-0' : 'pl-4'"
         >
-          <span class="font-weight-bold text-caption text-md-h6">
+          <span
+            class="custom-title-size justify-self-start"
+            :class="currentTab === index ? 'text-white font-weight-bold' : 'custom-color font-weight-regular'"
+          >
             {{ top.title }}
           </span>
         </v-tab>
@@ -39,6 +44,7 @@
 
     <v-tabs-window
       v-model="currentTab"
+      class="custom-color"
     >
       <v-tabs-window-item
         v-for="tab, index in tops[currentTab].contenuOnglet"
@@ -48,14 +54,14 @@
         role="tabpanel"
         :aria-labelledby="`tab-${index}`"
       >
-        <v-row class=" mb-4 mb-md-16 pb-md-16">
+        <v-row class=" mb-4 mb-md-10">
           <v-col
             v-for="top, f in tops[currentTab].contenuOnglet"
             :key="`${top.title}-${f}`"
             cols="6"
             md="3"
           >
-            <h5 class="text-h6 text-md-h4 font-weight-bold mb-4 mb-md-10 ">
+            <h5 class="custom-title-size  font-weight-bold mb-4 pb-md-16 ">
               {{ top.title }}
             </h5>
             <div class="d-flex flex-column ga-2">
@@ -65,7 +71,7 @@
                 :href="`${link.slug}`"
                 :external="link.slug.includes('http')"
                 :target="link.slug.includes('http') ? '_blank' : undefined"
-                class="text-white-light line-clamp-2 text-caption text-md-body-1 font-weight-regular"
+                class=" line-clamp-2 text-caption text-md-body-2 font-weight-regular pb-4"
               >
                 {{ link.title }}
               </NuxtLink>
@@ -96,5 +102,17 @@ const tops = await queryCollection('tops').all()
   -webkit-box-orient: vertical;
   -webkit-line-clamp: 2;
   line-clamp: 2;
+  font-weight: 500!important;
+}
+.custom-color:deep(a), .custom-color:deep(div){
+  color: #FFFFFF80!important;
+}
+@media (min-width: 600px) {
+  .custom-title-size {
+    font-size: 20px !important;
+    text-transform: none;
+    max-height: 50px!important;
+    line-height: 1.2!important;
+  }
 }
 </style>
