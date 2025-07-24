a7a91d0c8bb0459d89ee1ab6f61fd02937ccb15c
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2025-07-24 19:57:46 +0200

    hotfix: add missing btns in mobile header

--- components/HeaderOdysway.vue
+++ components/HeaderOdysway.vue
@@ -1,4 +1,78 @@
 <template>
+  <div
+    class="d-sm-none px-4 px-md-9 d-flex align-center height-app-bar custom-app-bar bg-white"
+    :class="!model ? 'app-bar-shadow' : ''"
+  >
+    <NuxtLink
+      :to="header.to"
+      class="header-logo-link"
+    >
+      <NuxtImg
+        preload
+        as="image"
+        format="webp"
+        quality="100"
+        :src="LogoOdyswayBleu"
+        width="320"
+        alt="Logo principale d'Odysway"
+        class="header-logo"
+      />
+    </NuxtLink>
+
+    <v-spacer />
+    <div class="d-flex align-center ga-4">
+      <v-btn
+        height="45"
+        color="primary"
+        rounded="default"
+        class="text-caption text-md-body-1 d-none d-md-inline"
+        @click="() => { router.push('/search'); captureOutboundLink(header.textButton1) }"
+      >
+        {{ header.textButton1 }}
+      </v-btn>
+      <v-btn
+        color="primary"
+        height="45"
+        class="text-caption text-md-body-1 d-none d-md-inline"
+        @click="() => { router.push('/a-propos'); captureOutboundLink(header.textButton2) }"
+      >
+        {{ header.textButton2 }}
+      </v-btn>
+      <v-btn-secondary
+        href="tel: +33184807975"
+        color="primary"
+        height="45"
+        variant="tonal"
+        rounded="default"
+        class="text-caption text-md-body-1 d-none d-md-inline"
+        @click="() => { trackPixel('trackCustom', 'ClickAppel'); captureOutboundLink(header.textButton3) }"
+      >
+        {{ header.textButton3 }}
+      </v-btn-secondary>
+      <v-btn
+        height="45"
+        color="white"
+        rounded="default"
+        class="text-caption text-md-body-1 d-none d-md-inline bg-primary"
+        @click="() => { router.push('/calendly'); trackPixel('trackCustom', 'ClickRDV'); captureOutboundLink(header.textButton4) }"
+      >
+        {{ header.textButton4 }}
+      </v-btn>
+      <v-btn
+        class="d-inline d-md-none "
+        icon
+        height="35"
+        variant="text"
+        aria-label="Menu"
+        :aria-expanded="model"
+        @click.stop="model = !model"
+      >
+        <v-icon>
+          {{ mdiMenu }}
+        </v-icon>
+      </v-btn>
+    </div>
+  </div>
   <v-app-bar
     elevation="0"
     mobile
