3528c37b457b5ccd212444c111e4455f59a2b003
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2025-07-28 08:49:03 +0200

    fix header manually

--- app.config.ts
+++ app.config.ts
@@ -2,7 +2,19 @@ export default defineAppConfig({
   seo: {
     siteName: 'Odysway | Agence spécialiste du voyage en immersion',
   },
-  header: 'Faisons connaissance 👋',
+  header: {
+    to: '/',
+    logo: {
+      desktop: '/logos/Logo-Odysway-Bleu.png',
+      mobile: '/logos/Logo-Odysway-Bleu.png',
+      alt: 'Odysway Logo',
+    },
+    search: true,
+    textButton1: 'Voyages',
+    textButton2: 'L\'esprit Odysway',
+    textButton3: '+33 1 84 80 79 75',
+    textButton4: 'Faisons connaissance 👋',
+  },
   footer: {
     logo: {
       image: '/logos/Logo-Odysway-Bleu.png',
