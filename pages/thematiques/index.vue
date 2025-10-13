763c1980f76db257be59c9566ec43286d903b3ac
Julien Leray <leray.jul@gmail.com>
2025-10-13 16:39:12 +0200

    adding thematiques

--- pages/thematiques/index.vue
+++ pages/thematiques/index.vue
@@ -13,24 +13,76 @@
 </template>
 
 <script setup>
-const { data: pageContent } = await useAsyncData('page-thematiques', () => {
-  return queryCollection('page_thematiques').first()
-})
+const sanity = useSanity()
 
-const { data: categories } = useAsyncData('categories', () => {
-  return queryCollection('categories').select('id', 'title', 'slug', 'discoveryTitle', 'image').all()
-})
+const pageContentQuery = groq`*[_type == "page_thematiques"][0]{
+  index,
+  slug,
+  common
+}`
 
-const { data: voyages } = useAsyncData('voyages-on-thematiques', () => {
-  return queryCollection('voyages').where('published', '=', true).all()
-})
+const categoriesQuery = groq`*[_type == "category"] | order(title asc) {
+  _id,
+  title,
+  slug,
+  discoveryTitle,
+  image
+}`
+
+const voyagesQuery = groq`*[_type == "voyage"] {
+  _id,
+  title,
+  slug,
+  image,
+  imageSecondary,
+  description,
+  duration,
+  nights,
+  level,
+  destinations[]-> {
+    _id,
+    title
+  },
+  experienceType-> {
+    _id,
+    title
+  },
+  categories[]-> {
+    _id,
+    title
+  },
+  rating,
+  comments,
+  pricing,
+  groupeAvailable,
+  privatisationAvailable,
+  customAvailable,
+  badgeSection,
+  monthlyAvailability,
+  idealPeriods,
+  miniatureDisplay
+}`
+
+const { data: pageContent } = await useAsyncData('page-thematiques', () =>
+  sanity.fetch(pageContentQuery)
+)
+
+const { data: categories } = await useAsyncData('categories', () =>
+  sanity.fetch(categoriesQuery)
+)
+
+const { data: voyages } = await useAsyncData('voyages-on-thematiques', () =>
+  sanity.fetch(voyagesQuery)
+)
 
 const categoriesWithVoyages = computed(() => {
-  if (!categories.value || voyages.value?.length === 0) return []
-  return categories.value?.map(category => ({
+  if (!categories.value || !voyages.value?.length) return []
+
+  return categories.value.map(category => ({
     ...category,
-    voyages: voyages.value?.filter(voyage =>
-      voyage.categories && voyage.categories?.some(c => c.name?.includes(category.slug)),
+    id: category._id,
+    voyages: voyages.value.filter(voyage =>
+      voyage.categories?.some(c => c._id === category._id)
     ),
   }))
 })
