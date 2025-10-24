0f1f0bb15a9e71f3742a7efb27eefcce493db8c1
Alexandre Ottmann <ottmann.alex@gmail.com>
2025-10-24 12:02:59 +0200

    Update getDateStatus.js

--- utils/getDateStatus.js
+++ utils/getDateStatus.js
@@ -1,23 +1,45 @@
 export function getDateStatus(date) {
-  if (date.displayed_status === 'soon_confirmed') {
-    return {
-      status: 'soon_confirmed',
-      text: 'Bientôt confirmé',
-      color: 'yellow',
+  if (!date.custom_display) {
+    if (date.max_travelers === date.booked_seat) {
+      return {
+        status: 'full',
+        text: 'Complet',
+        color: 'secondary',
+      }
+    }
+    if (date.booked_seat >= date.min_travelers) {
+      return {
+        status: 'confirmed',
+        text: 'Départ Garanti',
+        color: 'green',
+      }
+    }
+    else {
+      return {
+        status: 'soon_confirmed',
+        text: 'Bientôt confirmé',
+        color: 'yellow',
+      }
     }
   }
-
-  if (date.displayed_status === 'guaranteed' || date.max_travelers === date.booked_seat) {
-    return {
-      status: 'full',
-      text: 'Complet',
-      color: 'secondary',
-    }
-  }
-
-  return {
-    status: 'confirmed',
-    text: 'Départ Garanti',
-    color: 'green',
+  else {
+    const displayedStatusObject = [
+      {
+        status: 'soon_confirmed',
+        text: 'Bientôt confirmé',
+        color: 'yellow',
+      },
+      {
+        status: 'confirmed',
+        text: 'Départ Garanti',
+        color: 'green',
+      },
+      {
+        status: 'full',
+        text: 'Complet',
+        color: 'secondary',
+      },
+    ]
+    return displayedStatusObject.find(status => status.status === date.displayed_status)
   }
 }
