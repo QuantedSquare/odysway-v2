9c1be3abe15291a86237143df0dcda7c6ab729fa
Vladyslav Tezyk <vlad.tezyk.pro@gmail.com>
2025-11-07 13:49:13 +0100

    wip: add advanced search

--- server/api/v1/search/voyages/index.get.js
+++ server/api/v1/search/voyages/index.get.js
@@ -14,45 +14,69 @@ export default eventHandler(async (event) => {
   const query = getQuery(event)
   const searchTerm = query.keyword?.trim()
 
-  if (searchTerm && searchTerm.length > 0) {
-    const lowerCaseSearchTerm = searchTerm.toLowerCase()
+  // if (searchTerm && searchTerm.length > 0) {
+  //   const lowerCaseSearchTerm = searchTerm.toLowerCase()
 
-    const [destinations, regions, voyages] = await Promise.all([
-      sanityClient.fetch(`*[_type == "destination"]{
-        title,
-        "slug": slug.current
-      }`),
-      sanityClient.fetch(`*[_type == "region"]{
-        nom,
-        "slug": slug.current
-      }`),
-      sanityClient.fetch(`*[_type == "voyage" && (
+  //   const voyages = await
+  //   sanityClient.fetch(`*[_type == "voyage" && (
+  //       !('custom' in availabilityTypes) ||
+  //       (count(availabilityTypes) > 1)
+  //     )]{
+  //       title,
+  //       description,
+  //       "slug": slug.current,
+  //       experienceType->{
+  //        badgeTitle,
+  //       }
+  //     }`)
+
+  //   function filterAndMapData(data, dataSource) {
+  //     const searchKeywords = lowerCaseSearchTerm.split(/\s+/).filter(k => k.length > 0)
+
+  //     return data
+  //       .filter((item) => {
+  //         const fetchedText = (item.title || item.nom).toLowerCase() + item.description?.toLowerCase()
+  //         console.log('fetched text ', fetchedText)
+  //         const itemSlug = item.slug.toLowerCase()
+
+  //         return searchKeywords.some((keyword) => {
+  //           return fetchedText.includes(keyword) || itemSlug.includes(keyword)
+  //         })
+  //       })
+  //       .map(item => ({
+  //         title: item.title || item.nom,
+  //         slug: item.slug,
+  //         dataSource,
+  //       }))
+  //   }
+
+  //   const searchResults = [
+  //     ...filterAndMapData(voyages, 'voyages'),
+  //   ]
+
+  //   return searchResults
+  // }
+  if (searchTerm && searchTerm.length > 0) {
+    // Escape special characters to prevent injection issues and ensure the query is valid.
+    const cleanSearchTerm = searchTerm.replace(/["']/g, '\\$&')
+
+    const searchFilter = `[title, description, slug.current, destinations, experienceType, categories, authorNote ] match "*${cleanSearchTerm}*"`
+
+    const voyages = await sanityClient.fetch(`
+      *[_type == "voyage" && (${searchFilter}) && (
         !('custom' in availabilityTypes) ||
         (count(availabilityTypes) > 1)
       )]{
         title,
-        "slug": slug.current
-      }`),
-    ])
+        "slug": slug.current,
+      }
+    `)
 
-    function filterAndMapData(data, dataSource) {
-      return data
-        .filter((item) => {
-          const name = item.title || item.nom
-          return name.toLowerCase().includes(lowerCaseSearchTerm) || item.slug.includes(lowerCaseSearchTerm)
-        })
-        .map(item => ({
-          title: item.title || item.nom,
-          slug: item.slug,
-          dataSource,
-        }))
-    }
-
-    const searchResults = [
-      ...filterAndMapData(destinations, 'destinations'),
-      ...filterAndMapData(regions, 'regions'),
-      ...filterAndMapData(voyages, 'voyages'),
-    ]
+    const searchResults = voyages.map(item => ({
+      title: item.title,
+      slug: item.slug,
+      dataSource: 'voyages',
+    }))
 
     return searchResults
   }
