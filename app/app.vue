ee27d0beb628e9a8575ed5f89809019131c49d15
AlexandreOttmann <ottmann.alex@gmail.com>
2025-12-18 16:42:40 +0100

    changed hotjar id

--- app/app.vue
+++ app/app.vue
@@ -68,7 +68,7 @@ onMounted(() => {
       const script = document.createElement('script')
       script.innerHTML = `(function(h,o,t,j,a,r){
       h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-      h._hjSettings={hjid:6430819,hjsv:6};
+      h._hjSettings={hjid:4932189,hjsv:6};
       a=o.getElementsByTagName('head')[0];
       r=o.createElement('script');r.async=1;
       r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
