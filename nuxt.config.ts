e3a651064c6906865e4744da23aafe2b6e037739
AlexandreOttmann <ottmann.alex@gmail.com>
2026-02-23 10:05:14 +0100

    Replaced gtm script

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -42,16 +42,16 @@ export default defineNuxtConfig({
       script: [
         {
           id: 'gtm-script',
-          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-NP63ZR5');`,
+          innerHTML: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s);j.async=true;j.src="https://load.sst.odysway.com/28bwtluuzax.js?"+i;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','5qth5h1=EQVHMiEhWTkoV0kvJ1lSAUVTVERTCBpKFwUDBgINDVkbDhc%3D');`,
         },
       ],
-      noscript: [
-        {
-          id: 'gtm-noscript',
-          tagPosition: 'bodyOpen',
-          innerHTML: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NP63ZR5" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
-        },
-      ],
+      // noscript: [
+      //   {
+      //     id: 'gtm-noscript',
+      //     tagPosition: 'bodyOpen',
+      //     innerHTML: '<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-NP63ZR5" height="0" width="0" style="display:none;visibility:hidden"></iframe>',
+      //   },
+      // ],
 
     },
     pageTransition: { name: 'page', mode: 'out-in' },
