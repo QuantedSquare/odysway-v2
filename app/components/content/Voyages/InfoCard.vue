395952bd36ff86d06585ae362c51c304ff11f3e1
AlexandreOttmann <ottmann.alex@gmail.com>
2026-04-16 11:58:42 +0200

    Hotfix: duplicate attribute on failed merge

--- app/components/content/Voyages/InfoCard.vue
+++ app/components/content/Voyages/InfoCard.vue
@@ -138,9 +138,6 @@
                   <div
                     v-if="stickyBlock.ctaCall.avatars && stickyBlock.ctaCall.avatars.length > 0"
                     class="avatar-stack"
-                  <div
-                    v-if="stickyBlock.ctaCall.avatars && stickyBlock.ctaCall.avatars.length > 0"
-                    class="avatar-stack"
                   >
                     <v-avatar
                       v-for="(member, i) in stickyBlock.ctaCall.avatars.slice(0, 3)"
@@ -154,8 +151,6 @@
                         :alt="member.name || 'Team member'"
                         cover
                       />
-                    </v-avatar>
-                  </div>
                     </v-avatar>
                   </div>
 
@@ -237,7 +232,6 @@
 <script setup>
 import { mdiArrowRight, mdiCheckCircleOutline } from '@mdi/js'
 import { useGoTo } from 'vuetify'
-import { useGoTo } from 'vuetify'
 import dayjs from 'dayjs'
 
 const img = useImage()
@@ -255,7 +249,7 @@ const { stickyBlock, voyage } = defineProps({
     required: true,
   },
 })
-const img = useImage()
+
 const displayedDates = computed(() => {
   if (dates.value.length > 0) {
     const filteredDates = dates.value.filter(d => getDateStatus(d)?.status !== 'full')
