b7a7a607524f784777f896d8c36528837bffdf98
AlexandreOttmann <ottmann.alex@gmail.com>
2026-05-05 17:38:02 +0200

    Allow sanity url origin

--- server/middleware/visual-editing-headers.ts
+++ server/middleware/visual-editing-headers.ts
@@ -2,7 +2,7 @@ export default defineEventHandler((event) => {
   const config = useRuntimeConfig()
   if (config.public.environment === 'production') return
 
-  const origins = new Set(['http://localhost:3333'])
+  const origins = new Set(['http://localhost:3333', 'https://www.sanity.io/', 'https://odysway-studio.sanity.studio/'])
   if (process.env.SANITY_STUDIO_URL) origins.add(process.env.SANITY_STUDIO_URL)
   setResponseHeader(event, 'Content-Security-Policy', `frame-ancestors 'self' ${[...origins].join(' ')}`)
   removeResponseHeader(event, 'X-Frame-Options')
